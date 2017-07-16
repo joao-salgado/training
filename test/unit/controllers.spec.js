1abf61920ae15a432cd0929facdba4843dee74d5
JoaoGabrielSalgado <jgs1884@gmail.com>
2017-07-16 14:32:58 -0300

    initial commit

---
+++ test/unit/controllers.spec.js
@@ -0,0 +1,17 @@
+'use strict'
+
+describe('controller specs', function() {
+	var $scope;
+
+	beforeEach(module('myApp.controllers'));
+
+	beforeEach(inject(function($rootScope, $controller) {
+		$scope = $rootScope.$new();
+		$controller('helloWorldCtrl', {$scope: $scope});
+	}));
+
+	it('should create "name" model with first name "Jane"', function() {
+		expect($scope.name.first).toBe("Jane");
+	});
+
+});
